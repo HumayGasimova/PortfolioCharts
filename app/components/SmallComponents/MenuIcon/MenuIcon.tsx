eac28b69564559e08c0f7bdd1ee9b7b86f3472a3
HumayMustaf <humaymustaf@bestcomp.net>
2025-03-04 14:12:47 +0400

    Toolbar component search field in progress...

--- app/components/SmallComponents/MenuIcon/MenuIcon.tsx
+++ app/components/SmallComponents/MenuIcon/MenuIcon.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import styles from './MenuIcons.module.scss';
 
 export default function MenuIcon() {
+
+  const onMenuIconClick = () => {
+  }
+
   return (
-    <div className={styles.menuIconWrapper}>
+    <div className={styles.menuIconWrapper} onClick={onMenuIconClick}>
         <div className={styles.menuIconLongLine}/>
         <div className={styles.menuIconShortLine}/>
         <div className={styles.menuIconLongLine}/>
--- app/components/Toolbar/Toobar.tsx
+++ app/components/Toolbar/Toobar.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useForm, SubmitHandler } from "react-hook-form"
 
 import styles from './Toolbar.module.scss';
 import MenuIcon from '../SmallComponents/MenuIcon/MenuIcon';
 
+import { 
+  FontAwesomeIcon 
+} from '@fortawesome/react-fontawesome';
+
+import { 
+  faSearch
+} from '@fortawesome/free-solid-svg-icons';
+
+type Inputs = {
+  example: string
+  exampleRequired: string
+}
+
 export default function Toobar() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  console.log(watch("example")) // watch input value by passing the name of it
+
   return (
     <div className={styles.toolbarContainerWrapper}>
       <div className={styles.icon}>
@@ -13,7 +37,18 @@ export default function Toobar() {
       <div className={styles.toolbarWrapper}>
         <div className={styles.toolbarLeftPartWrapper}>
           <MenuIcon/>
-          <div>Search</div>
+          <div className={styles.searchFieldWrapper}>
+             <form onSubmit={handleSubmit(onSubmit)}>
+              <input className={styles.searchField} defaultValue="Search for anything..." {...register("example")} />
+              <FontAwesomeIcon 
+              icon={faSearch}
+              color='rgb(165, 165, 165)'
+              size='sm'
+            />
+            </form>
+          
+          </div>
+      
         </div>
         <div className={styles.toolbarRightPartWrapper}>
           <div>List of icons</div>
